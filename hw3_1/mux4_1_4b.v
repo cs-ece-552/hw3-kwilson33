ea870b18c37b8c8305a73bedbcfca44ae00b9946
Kevin Wilson <kwilson33@wisc.edu>
2019-02-14 21:50:31 -0600

    fix

--- hw3_1/mux4_1_4b.v
+++ hw3_1/mux4_1_4b.v
@@ -9,16 +9,15 @@ module mux4_1_4b(InA, InB, InC, InD, S, Out);
     // parameter N for length of inputs and outputs (to use with larger inputs/outputs)
     parameter N = 4;
 
-    input [N-1:0]   InA, InB, InC, InD;
-    input [1:0]     S;
+    input  [N-1:0]   InA, InB, InC, InD;
+    input  [1:0]     S;
     output [N-1:0]  Out;
 
-	// use a generate variable to instantiate as many modules as needed. If N was 5 
- 	// and the inputs and outputs were 5 bits long it would make 5 muxes no problem!
-	genvar i;
-	for (i=0; i < N; i=i+1) begin
-		mux4_1 quadMux(InA[i], InB[i], InC[i], InD[i], S, Out[i]);
-	end
+	
+	mux4_1 quadMux0(InA[N-4], InB[N-4], InC[N-4], InD[N-4], S, Out[N-4]);
+	mux4_1 quadMux1(InA[N-3], InB[N-3], InC[N-3], InD[N-3], S, Out[N-3]);
+	mux4_1 quadMux2(InA[N-2], InB[N-2], InC[N-2], InD[N-2], S, Out[N-2]);
+	mux4_1 quadMux3(InA[N-1], InB[N-1], InC[N-1], InD[N-1], S, Out[N-1]);
 
 
 endmodule
